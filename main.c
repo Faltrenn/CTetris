d9822ffd61525440124c1ff23265b05758db12fd
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-10-24 15:02:04 -0300

    Adição de menus.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <curses.h>
+#include <ncurses.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -206,7 +206,7 @@ void down_piece(Piece * piece, char **map, char *options[], WINDOW *w) {
     }
 }
 
-int main() {
+void menu_jogar() {
     initscr();
     cbreak();
     noecho();
@@ -293,6 +293,65 @@ int main() {
     }
 
     endwin();
+}
+
+void main_menu() {
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, 0);
+    curs_set(0);
+
+    keypad(stdscr, TRUE);
+
+    start_color();
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+
+    char *options[] = {
+        "Jogar",
+        "Sair",
+        NULL
+    };
+
+    int selected = 0;
+    while(1) {
+        clear();
+        int i = 0;
+        for(i = 0; options[i] != NULL; i++) {
+            if(i == selected) {
+                attron(COLOR_PAIR(1));
+                mvprintw(LINES/2-2+i, COLS/2 - strlen(options[i])/2, "%s", options[i]);
+                attroff(COLOR_PAIR(1));
+            } else {
+                mvprintw(LINES/2-2+i, COLS/2 - strlen(options[i])/2, "%s", options[i]);
+            }
+        }
+        
+        int key = getch();
+        if(key == KEY_DOWN) {
+            if(selected < i-1) {
+                selected++;
+            }
+        } else if(key == KEY_UP) {
+            if(selected > 0) {
+                selected--;
+            }
+        } else if(key == '\n') {
+            if(strcmp(options[selected], "Jogar") == 0) {
+                menu_jogar();
+            } else if(strcmp(options[selected], "Sair") == 0) {
+                break;
+            }
+        }
+
+        refresh();
+    }
+
+    endwin();
+}
+
+int main() {
+    main_menu();
 
     return 0;
 }
